1a3dab9b83fd54d299bf135ad634a07698d0d28c
harrycomeon <15265323451@163.com>
2019-07-17 16:28:29 +0800

    [your_comment]

---
+++ ur5_ikfast_manipulator_plugin/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py ur5 manipulator ur5_ikfast_manipulator_plugin /home/comeon-harry/at_ur5/src/ur5_ikfast_manipulator_plugin/src/ur5_manipulator_ikfast_solver.cpp
