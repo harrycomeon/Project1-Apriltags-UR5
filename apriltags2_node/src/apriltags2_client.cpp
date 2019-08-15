0dfd35a54999e4feccfb1502d8775c4fed7062c5
harrycomeon <15265323451@163.com>
2019-08-15 08:38:19 +0800

    Update apriltags2_client.cpp

--- apriltags2_node/src/apriltags2_client.cpp
+++ apriltags2_node/src/apriltags2_client.cpp
@@ -4,18 +4,12 @@
 
 #include <moveit/move_group_interface/move_group_interface.h>
 
-#include <actionlib/client/simple_action_client.h>
-#include <control_msgs/FollowJointTrajectoryAction.h>
-#include <apriltags2_node/PlanCartesianPath.h>
-
-
 class ScanNPlan
 {
 public:
-  ScanNPlan(ros::NodeHandle& nh) : ac_("arm_controller/follow_joint_trajectory", true)//joint_trajectory_action
+  ScanNPlan(ros::NodeHandle& nh)
   {
     vision_client_ = nh.serviceClient<apriltags2_node::LocalizePart>("localize_part");
-    cartesian_client_ = nh.serviceClient<apriltags2_node::PlanCartesianPath>("plan_path");
   }
 
   void start(const std::string& base_frame)
@@ -25,7 +19,7 @@ public:
     apriltags2_node::LocalizePart srv;
 
     srv.request.base_frame = base_frame;
-    ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
+    //ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
     //如果服务回调不成功
     if(!vision_client_.call(srv))
     {
@@ -33,48 +27,57 @@ public:
       return ;
     }
 
-    ROS_INFO_STREAM("part localized: " << srv.response);
-    //ROS_INFO_STREAM("frame_id: " << srv.request.base_frame);
-    //LocalizePart服务的响应后来初始化一个新move_target变量
-    //ROS_INFO_STREAM("part localized: " << srv.response.pose.orientation);
-    //srv.response.pose.orientation.z=(-1)*srv.response.pose.orientation.z;
-    //srv.response.pose.orientation.y=(-1)*srv.response.pose.orientation.y;
-    //srv.response.pose.orientation.x=(-1)*srv.response.pose.orientation.x;
-    //ROS_INFO_STREAM("part localized: " << srv.response.pose.orientation);
-    //交换了y，z轴，实现了机械手朝下
+   // ROS_INFO_STREAM("part localized: " << srv.response);
+
     move_target = srv.response.pose;
-    move_target1=srv.response.pose;
-    move_target.orientation.y=move_target1.orientation.z;
-    move_target.orientation.z=move_target1.orientation.y;
- 
-    //ROS_INFO_STREAM("part localized: " << move_target.orientation);
+    move_target1 = srv.response.pose;
+   // move_target.position.z=move_target1.position.z+0.15;
+   // move_target.position.y=move_target1.position.y+0.045;
+   // move_target.position.x=move_target1.position.x-0.030;
+
+   //绕x轴逆时针变化90度
+  /*
+    move_target.orientation.x=0.70710678119*move_target1.orientation.x+0.70710678119*move_target1.orientation.w;
+    move_target.orientation.y=0.70710678119*move_target1.orientation.y-0.70710678119*move_target1.orientation.z;
+    move_target.orientation.z=0.70710678119*move_target1.orientation.z+0.70710678119*move_target1.orientation.y;
+    move_target.orientation.w=-0.70710678119*move_target1.orientation.x+0.70710678119*move_target1.orientation.w;
+ */
+
+    //ROS_INFO_STREAM("move_target: " << move_target);
+
     //创建一个对象，move_group（）构造函数定义计划组名称
     moveit::planning_interface::MoveGroupInterface move_group("manipulator");
 
+    moveit::planning_interface::MoveItErrorCode success;
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+
+     //设置初始位置
+    move_group.setStartState(*move_group.getCurrentState());
+
     // Plan for robot to move to part
     //使用move_group对象的setPoseTarget功能设置所需的笛卡尔目标位置
+    //Get the name of the end-effector link
+    //end_effector_link =move_group.getEndEffectorLink();
+    //Allow replanning to increase the odds of a solution
+    //move_group.allowReplanning(True);
+    // Allow some leeway in position (meters) and orientation (radians)
+
+    //move_group.setGoalPositionTolerance(0.01);
+    //move_group.setGoalOrientationTolerance(0.05);
     move_group.setPoseReferenceFrame(base_frame);
+    //move_group.setStartStateToCurrentState();
     move_group.setPoseTarget(move_target); 
-    move_group.move();
+    //move_group.setPlannerId("RRTConnectkConfigDefault");
+/*
+    move_group.setPlanningTime(10);
+    //进行运动规划
+    success = move_group.plan(plan);   //运动规划输出
+    ROS_INFO("Visualizing plan (stateCatch pose) %s",success == moveit_msgs::MoveItErrorCodes::SUCCESS ? "SUCCESS" : "FAILED");
+    if (success  == moveit_msgs::MoveItErrorCodes::SUCCESS)  
+    move_group.execute(plan);
+    move_group.setStartState(*move_group.getCurrentState());
 
-
-    // Plan cartesian path
-    apriltags2_node::PlanCartesianPath cartesian_srv;
-    cartesian_srv.request.pose = move_target;
-    if (!cartesian_client_.call(cartesian_srv))
-    {
-      ROS_ERROR("Could not plan for path");
-      return;
-    }
-
-    // Execute descartes-planned path directly (bypassing MoveIt)
-    ROS_INFO("Got cart path, executing");
-    control_msgs::FollowJointTrajectoryGoal goal;
-    goal.trajectory = cartesian_srv.response.trajectory;
-    ac_.sendGoal(goal);
-    ac_.waitForResult();
-    ROS_INFO("Done");
-
+*/    move_group.move();
   }
   
 private:
@@ -82,8 +85,7 @@ private:
   ros::ServiceClient vision_client_;
   geometry_msgs::Pose move_target;
   geometry_msgs::Pose move_target1;
-  ros::ServiceClient cartesian_client_;
-  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac_;
+  std::string end_effector_link;
 };
 
 int main(int argc, char **argv)
@@ -91,25 +93,20 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "apriltags2_node");
   ros::NodeHandle nh;
   ros::NodeHandle private_node_handle ("~");
-
-  //如所描述的在这里，所述move_group.move()命令需要使用“异步”旋转器，以允许阻塞期间ROS消息的处理move()命令
   ros::AsyncSpinner async_spinner(1);
   async_spinner.start();
 
-  while(ros::ok())
-  {
+  //ros::Rate r(10); // 10 hz,它的功能就是先设定一个频率，然后通过睡眠度过一个循环中剩下的时间，来达到该设定频率。
     ROS_INFO("ScanNPlan node has been initialized");
     std::string base_frame;
-    private_node_handle.param<std::string>("base_frame", base_frame, "world"); // parameter name, string object reference, default value
-
+    private_node_handle.param<std::string>("base_frame", base_frame, "world"); // world parameter name, string object reference, default value
     ScanNPlan app(nh);
     ros::Duration(.5).sleep();  // wait for the class to initialize
+  while(ros::ok())
+  {
+    
     app.start(base_frame);
-    ros::Duration(.1000).sleep();
-    ros::Duration(.1000).sleep();
     //ros::waitForShutdown();
-    //ros::spinOnce();
-    ros::Duration(.5).sleep();
   }
   return 0;
 }
